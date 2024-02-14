b6507b308fc722fd1f0b105e4eb06a26734d303b
Mustafa Demiroglu <mustafa.demiroglu@gmx.de>
2024-02-14 14:45:15 +0100

    datentypen

---
+++ intelliJ_FrauMitTulius/PythonLernenMitFrauTulius/Demodateien/d_01_konsole.py
@@ -0,0 +1,13 @@
+# Konsolenausgabe
+print("Hallo \n")
+print("Welt!")
+
+# Konsoleneingabe
+# input
+# input ("Ihr Alter: ")
+
+# Eingabe wird in Variable alter gespeichert
+alter = input("Ihr Alter: ")
+
+# Stringverknüpfung mit Variable alter
+print("Sie sind", alter, "Jahre alt.")
---
+++ intelliJ_FrauMitTulius/PythonLernenMitFrauTulius/Demodateien/d_02_datentypen.py
@@ -0,0 +1,31 @@
+# integer
+menge = 5
+# float
+preis = 1.50
+# gesamtpreis automatisch float
+gesamtpreis = menge*preis
+
+print("Der Gesamtpreis beträgt: ", gesamtpreis)
+# string / Zeichen
+produkt = "Hundefutter"
+
+print("Der Preis für ", produkt, "beträgt ", gesamtpreis)
+
+# Hinweis: in anderen Sprachen true/false kleingescrieben
+zufriedenheit = False
+
+print(zufriedenheit)
+
+# Bedingungen schauen wir uns später genauer an
+if zufriedenheit:
+    print("zufriedener Hund")
+else:
+    print("unzufriedener Hund")
+
+# Listen / Array
+lottozahlen = ["Hase", 3, 17, 42, 33, 5]
+print(lottozahlen[1])
+
+
+
+
